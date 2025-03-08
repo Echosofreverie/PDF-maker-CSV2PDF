3f1170c25a4ff50ac65da0b667f9ef5ffe946df9
AnnaLIU-8987 <anliu6-C@my.cityu.edu.hk>
2025-03-08 11:58:52 +0800

    3/8 Add lines

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 
 pdf = FPDF(orientation="P", unit="mm", format="A4")
+pdf.set_auto_page_break(auto=False,margin=0)
 df = pd.read_csv("topics.csv")
 for index,row in df.iterrows():
 
@@ -10,9 +11,25 @@ for index,row in df.iterrows():
     pdf.set_font(family="Times",style="B",size=24)#B加粗
     pdf.set_text_color(100,100,100)#灰色RGB
     pdf.cell(w = 0,h=12,txt=row["Topic"],align="L",ln=1)#ln占一行
-    pdf.line(10,22,200,22)
+    for y in range(20,295,10):
+        pdf.line(10,y,200,y)
+
+
+
+    #添加页面脚标 set the footer
+    pdf.ln(265)
+    pdf.set_font(family="Times",style="I",size=8)
+    pdf.set_text_color(180,180,180)
+    pdf.cell(w=0,h=10,txt=row["Topic"],align="R")
     for i in range(row["Pages"]-1):
         pdf.add_page()
+
+        pdf.ln(277)
+        pdf.set_font(family="Times", style="I", size=8)
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+        for y in range(10, 292, 10):
+            pdf.line(10, y, 200, y)
 
 
 
